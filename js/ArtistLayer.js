84948a8b0a8fe14cfb172c2edcaf7e39b74c9e2b
VanBronckhorst <filippo.pellolio@mail.polimi.it>
2015-11-07 15:58:20 -0600

    whole application skeleton

--- js/ArtistLayer.js
+++ js/ArtistLayer.js
@@ -10,19 +10,20 @@ var ArtistLayer = L.Class.extend({
 
         // create a DOM element and put it into one of the map panes
         this._el = L.DomUtil.create('div', 'my-custom-layer leaflet-zoom-hide');
-        map.getPanes().overlayPane.appendChild(this._el);
+        map.getPanes().markerPane.appendChild(this._el);
         
         this.svg = d3.select(this._el).append("svg")
         // Find a way to get the size 
-        this.svg.style("position","absolute").style("left","-25px").style("top","-25px").style("width","50px").style("height","50px").attr("viewBox","0 0 50 50")
+        this.svg.style("position","relative").style("left","-25px").style("top","-25px").style("width","50px").style("height","50px").attr("viewBox","0 0 50 50")
 		
 		this.svg.append("circle").style("fill","#779ECB").attr("cx", 25)
         .attr("cy", 25)
         .attr("r", 25)
-		
+
+        this.update();
         // add a viewreset event listener for updating layer's position, do the latter
-        map.on('viewreset', this._reset, this)
-        this._reset();
+        map.on('zoom viewreset', this.update, this);
+
     },
 
     onRemove: function (map) {
@@ -31,9 +32,10 @@ var ArtistLayer = L.Class.extend({
         map.off('viewreset', this._reset, this);
     },
 
-    _reset: function () {
+    update: function () {
         // update layer's position
         var pos = this._map.latLngToLayerPoint(this._latlng);
+        console.log(this._latlng,pos)
         L.DomUtil.setPosition(this._el, pos);
     }
 });
