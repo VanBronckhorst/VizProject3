63fefddfd6265d5e47ec6dfce8b982d5a66d8e46
VanBronckhorst <filippo.pellolio@mail.polimi.it>
2015-11-11 19:36:42 -0600

    new data

--- app/script/artistArrayCreator.py
+++ app/script/artistArrayCreator.py
@@ -3,14 +3,22 @@ __author__ = 'Filippo'
 
 import csv
 
-artists = []
+artistsPerYear = {}
 
 for i in range(1940,2000):
     with open("../data/bestArtistsByYear/"+str(i)+'artist.csv', 'rb') as csvfile:
+        year=[]
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        spamreader.next()
         for row in spamreader:
             if row[0] != "Artist":
-                artists.append(row[0])
+                year.append({"name":row[0].decode('latin-1').encode("utf-8"),"value":row[1]})
+        artistsPerYear[i]=year
 
-print artists
-print len(artists)
+print artistsPerYear
+
+print len(artistsPerYear)
+
+import json
+with open("../data/artistsPopularityPerYear", 'w') as csvfile:
+    json.dump(artistsPerYear,csvfile)
