67ea5f9634dad65cbd88465b34f1c7bbfda9a4b6
Jeeeck <gscola2@uic.edu>
2015-11-19 05:08:48 +0100

    Added check on previously added artist/genre

--- app/js/TimeLine/TimelineColors.js
+++ app/js/TimeLine/TimelineColors.js
@@ -11,12 +11,12 @@ var staticTimelineColors = [ "#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E
 
 staticTimelineColors = _.shuffle( staticTimelineColors );
 
-var dynamicTimelineColorsBlue = [ "#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E6",
-								  "#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E6" ];
+var dynamicTimelineColorsBlue = [ "#033C99", "#0341A5", "#0346B2", "#034BBF", "#0450CC",
+								  "#0455D8", "#045AE5", "#045FF2", "#0061FF", "#0564FF" ];
 
-var dynamicTimelineColorsRed = [ "#B30000", "#E34A33", "#FC8D59", "#FDBB84", "#FDD49E",
-								 "#B30000", "#E34A33", "#FC8D59", "#FDBB84", "#FDD49E" ];
+var dynamicTimelineColorsRed = [ "#B30000", "#BF0000", "#CC0000", "#D80000", "#E50000",
+								 "#F20000", "#FF0000", "#FA2200", "#FC2B00", "#F64300" ];
 
-var dynamicTimelineColorsMixed = [ "#980043", "#DD1C77", "#DF65B0", "#C994C7", "#D4B9DA",
-							       "#980043", "#DD1C77", "#DF65B0", "#C994C7", "#D4B9DA" ];
+var dynamicTimelineColorsMixed = [ "#980043", "#A50047", "#B2004D", "#BF0052", "#CC0058",
+							       "#D8005D", "#E50063", "#F20068", "#FF006E", "#FC0D6E" ];
 
